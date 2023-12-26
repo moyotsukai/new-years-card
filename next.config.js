63bc7554cd4a00d4c9fd60cd1a75b6139b29025e
moyotsukai <gyoshu.bowie@gmail.com>
2023-12-27 04:49:23 +0900

    add: sub directory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  assetPrefix: "/newyearscard",
+  basePath: "/newyearscard"
+}
 
 module.exports = nextConfig
