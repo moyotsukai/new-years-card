fbdc043472b72d846f86f87ef6af3979ed30d294
moyotsukai <gyoshu.bowie@gmail.com>
2023-12-27 05:13:24 +0900

    change: sub domain

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = process.env.NODE_ENV == "production"
-
-const nextConfig = {
-  assetPrefix: isProduction ? "/newyearscard" : "",
-  basePath: isProduction ? "/newyearscard" : ""
-}
+const nextConfig = {}
 
 module.exports = nextConfig
